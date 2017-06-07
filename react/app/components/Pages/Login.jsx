c1b35287b6c4f98cdce256e7b082948e31a3509b
Gustavo Roríguez <gustavrod@gmail.com>
2017-06-07 16:56:04 +0200

    Improvements to login/register

--- react/app/components/Pages/Login.jsx
+++ react/app/components/Pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { Grid, Row, Col, Panel, Button } from "react-bootstrap";
 import { Router, Route, Link, History } from "react-router";
 
@@ -7,15 +8,9 @@ import { Router, Route, Link, History } from "react-router";
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loading: true
-    };
     this.handleEmail = this.handleEmail.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  componentDidMount() {
-    this.setState({ loading: false });
   }
   handleEmail(event) {
     this.setState({ email: event.target.value });
@@ -25,141 +20,115 @@ class Login extends React.Component {
   }
   handleSubmit(e) {
     e.preventDefault();
-    // set app to loading
-    this.setState({ loading: true });
-    // construct post data
-    var formData = new FormData();
-    formData.append("email", this.state.email);
-    formData.append("password", this.state.password);
-    // make request
-    fetch(SERVER_ADDR + "/login", {
-      method: "POST",
-      body: formData
-    }).then(result => {
-      if (result.status == 401) {
-        result.text().then(text => {
-          alert(text);
-          this.setState({ loading: false });
-        });
-      } else {
-        result.text().then(text => {
-          let date = new Date();
-          date.setTime(date.getTime() + 100 * 24 * 60 * 60 * 1000);
-          // set cookie
-          document.cookie = [
-            "science={access_token: " + text + " }",
-            "; expires=" + date.toUTCString(),
-            "; path=/",
-            "; domain=" + SERVER_DOMAIN
-          ].join("");
-          // navigate to /dashboard
-          document.location =
-            "http://" + SERVER_DOMAIN + ":" + SERVER_PORT + "/dashboard";
-        });
-      }
-    });
+    let instance = $(ReactDOM.findDOMNode(this.form)).parsley();
+    if (instance.isValid()) {
+      // construct post data
+      var formData = new FormData();
+      formData.append("email", this.state.email);
+      formData.append("password", this.state.password);
+      // make request
+      fetch(SERVER_ADDR + "/login", {
+        method: "POST",
+        body: formData
+      }).then(result => {
+        if (result.status == 401) {
+          result.text().then(text => {
+            alert(text); // wrong password or email not found
+          });
+        } else {
+          result.text().then(text => {
+            let date = new Date();
+            date.setTime(date.getTime() + 100 * 24 * 60 * 60 * 1000);
+            // set cookie
+            document.cookie = [
+              "science={access_token: " + text + " }",
+              "; expires=" + date.toUTCString(),
+              "; path=/",
+              "; domain=" + SERVER_DOMAIN
+            ].join("");
+            // navigate to /dashboard
+            document.location =
+              "http://" + SERVER_DOMAIN + ":" + SERVER_PORT + "/dashboard";
+          });
+        }
+      });
+    }
   }
   render() {
-    if (this.state.loading) {
-      return (
-        <div className="block-center mt-xl wd-xl">
-          {/* START panel */}
-          <div className="panel panel-dark panel-flat">
-            <div className="panel-heading text-center">
-              <a href="#">
-                <img
-                  src="img/science1.png"
-                  alt="Image"
-                  className="block-center img-rounded"
+    return (
+      <div className="block-center mt-xl wd-xl">
+        {/* START panel */}
+        <div className="panel panel-dark panel-flat">
+          <div className="panel-heading text-center">
+            <a href="#">
+              <img
+                src="img/science1.png"
+                alt="Image"
+                className="block-center img-rounded"
+              />
+            </a>
+          </div>
+          <div className="panel-body">
+            <p className="text-center pv">SIGN IN TO CONTINUE.</p>
+            <form
+              ref={form => {
+                this.form = form;
+              }}
+              data-parsley-validate=""
+              onSubmit={this.handleSubmit}
+              id="login-form"
+              noValidate
+              className="mb-lg"
+            >
+              <div className="form-group has-feedback">
+                <input
+                  onChange={this.handleEmail}
+                  id="exampleInputEmail1"
+                  type="email"
+                  placeholder="Enter email"
+                  autoComplete="off"
+                  required="required"
+                  className="form-control"
                 />
-              </a>
-            </div>
-            <div className="panel-body">
-              <p className="text-center pv">PROCESSING TRANSACTION...</p>
-              <div className="jumbotron">
-                <div className="panel-body whirl duo" />
+                <span className="fa fa-envelope form-control-feedback text-muted" />
               </div>
-            </div>
-          </div>
-          {/* END panel */}
-        </div>
-      );
-    } else {
-      return (
-        <div className="block-center mt-xl wd-xl">
-          {/* START panel */}
-          <div className="panel panel-dark panel-flat">
-            <div className="panel-heading text-center">
-              <a href="#">
-                <img
-                  src="img/science1.png"
-                  alt="Image"
-                  className="block-center img-rounded"
+              <div className="form-group has-feedback">
+                <input
+                  onChange={this.handlePassword}
+                  id="exampleInputPassword1"
+                  type="password"
+                  placeholder="Password"
+                  required="required"
+                  className="form-control"
                 />
-              </a>
-            </div>
-            <div className="panel-body">
-              <p className="text-center pv">SIGN IN TO CONTINUE.</p>
-              <form
-                onSubmit={this.handleSubmit}
-                role="form"
-                data-parsley-validate=""
-                noValidate
-                className="mb-lg"
-              >
-                <div className="form-group has-feedback">
-                  <input
-                    onChange={this.handleEmail}
-                    id="exampleInputEmail1"
-                    type="email"
-                    placeholder="Enter email"
-                    autoComplete="off"
-                    required="required"
-                    className="form-control"
-                  />
-                  <span className="fa fa-envelope form-control-feedback text-muted" />
+                <span className="fa fa-lock form-control-feedback text-muted" />
+              </div>
+              <div className="clearfix">
+                <div className="checkbox c-checkbox pull-left mt0">
+                  <label>
+                    <input type="checkbox" value="" name="remember" />
+                    <em className="fa fa-check" />Remember Me
+                  </label>
                 </div>
-                <div className="form-group has-feedback">
-                  <input
-                    onChange={this.handlePassword}
-                    id="exampleInputPassword1"
-                    type="password"
-                    placeholder="Password"
-                    required="required"
-                    className="form-control"
-                  />
-                  <span className="fa fa-lock form-control-feedback text-muted" />
+                <div className="pull-right">
+                  <Link to="recover" className="text-muted">
+                    Forgot your password?
+                  </Link>
                 </div>
-                <div className="clearfix">
-                  <div className="checkbox c-checkbox pull-left mt0">
-                    <label>
-                      <input type="checkbox" value="" name="remember" />
-                      <em className="fa fa-check" />Remember Me
-                    </label>
-                  </div>
-                  <div className="pull-right">
-                    <Link to="recover" className="text-muted">
-                      Forgot your password?
-                    </Link>
-                  </div>
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-block btn-primary mt-lg"
-                >
-                  Login
-                </button>
-              </form>
-              <p className="pt-lg text-center">Need to Signup?</p>
-              <Link to="register" className="btn btn-block btn-default">
-                Register Now
-              </Link>
-            </div>
+              </div>
+              <button type="submit" className="btn btn-block btn-primary mt-lg">
+                Login
+              </button>
+            </form>
+            <p className="pt-lg text-center">Need to Signup?</p>
+            <Link to="register" className="btn btn-block btn-default">
+              Register Now
+            </Link>
           </div>
-          {/* END panel */}
         </div>
-      );
-    }
+        {/* END panel */}
+      </div>
+    );
   }
 }
 
--- react/app/components/Pages/Register.jsx
+++ react/app/components/Pages/Register.jsx
@@ -1,19 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { Grid, Row, Col, Panel, Button } from "react-bootstrap";
 import { Router, Route, Link, History } from "react-router";
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loading: true
-    };
     this.handleEmail = this.handleEmail.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  componentDidMount() {
-    this.setState({ loading: false });
   }
   handleEmail(event) {
     this.setState({ email: event.target.value });
@@ -23,169 +18,138 @@ class Register extends React.Component {
   }
   handleSubmit(e) {
     e.preventDefault();
-    // set app to loading
-    this.setState({ loading: true });
-    // construct post data
-    var formData = new FormData();
-    formData.append("email", this.state.email);
-    formData.append("password", this.state.password);
-    // make request
-    fetch(SERVER_ADDR + "/register", {
-      method: "POST",
-      body: formData
-    }).then(result => {
-      result.text().then(text => {
-        if (text.length < 50) {
-          // email taken
-          alert(text);
-          this.setState({ loading: false });
-        } else {
-          let date = new Date();
-          date.setTime(date.getTime() + 100 * 24 * 60 * 60 * 1000);
-          // set cookie
-          document.cookie = [
-            "science={access_token: " + text + " }",
-            "; expires=" + date.toUTCString(),
-            "; path=/",
-            "; domain=" + SERVER_DOMAIN
-          ].join("");
-          // navigate to /dashboard
-          document.location =
-            "http://" + SERVER_DOMAIN + ":" + SERVER_PORT + "/dashboard";
-        }
+    let instance = $(ReactDOM.findDOMNode(this.form)).parsley();
+    if (instance.isValid()) {
+      // construct post data
+      var formData = new FormData();
+      formData.append("email", this.state.email);
+      formData.append("password", this.state.password);
+      // make request
+      fetch(SERVER_ADDR + "/register", {
+        method: "POST",
+        body: formData
+      }).then(result => {
+        result.text().then(text => {
+          if (text.length < 50) {
+            // email taken
+            alert(text);
+          } else {
+            let date = new Date();
+            date.setTime(date.getTime() + 100 * 24 * 60 * 60 * 1000);
+            // set cookie
+            document.cookie = [
+              "science={access_token: " + text + " }",
+              "; expires=" + date.toUTCString(),
+              "; path=/",
+              "; domain=" + SERVER_DOMAIN
+            ].join("");
+            document.location =
+              "http://" + SERVER_DOMAIN + ":" + SERVER_PORT + "/dashboard";
+          }
+        });
       });
-    });
+    }
   }
   render() {
-    if (this.state.loading) {
-      return (
-        <div className="block-center mt-xl wd-xl">
-          {/* START panel */}
-          <div className="panel panel-dark panel-flat">
-            <div className="panel-heading text-center">
-              <a href="#">
-                <img
-                  src="img/science1.png"
-                  alt="Image"
-                  className="block-center img-rounded"
+    return (
+      <div className="block-center mt-xl wd-xl">
+        {/* START panel */}
+        <div className="panel panel-dark panel-flat">
+          <div className="panel-heading text-center">
+            <a href="#">
+              <img
+                src="img/science1.png"
+                alt="Image"
+                className="block-center img-rounded"
+              />
+            </a>
+          </div>
+          <div className="panel-body">
+            <p className="text-center pv">SIGNUP TO GET INSTANT ACCESS</p>
+            <form
+              ref={form => {
+                this.form = form;
+              }}
+              data-parsley-validate=""
+              onSubmit={this.handleSubmit}
+              id="register-form"
+              noValidate
+              className="mb-lg"
+            >
+              <div className="form-group has-feedback">
+                <label htmlFor="signupInputEmail1" className="text-muted">
+                  Email address
+                </label>
+                <input
+                  onChange={this.handleEmail}
+                  id="signupInputEmail1"
+                  type="email"
+                  placeholder="Enter email"
+                  autoComplete="off"
+                  required="required"
+                  className="form-control"
                 />
-              </a>
-            </div>
-            <div className="panel-body">
-              <p className="text-center pv">PROCESSING TRANSACTION...</p>
-              <div className="jumbotron">
-                <div className="panel-body whirl duo" />
+                <span className="fa fa-envelope form-control-feedback text-muted" />
               </div>
-            </div>
-          </div>
-          {/* END panel */}
-        </div>
-      );
-    } else {
-      return (
-        <div className="block-center mt-xl wd-xl">
-          {/* START panel */}
-          <div className="panel panel-dark panel-flat">
-            <div className="panel-heading text-center">
-              <a href="#">
-                <img
-                  src="img/science1.png"
-                  alt="Image"
-                  className="block-center img-rounded"
+              <div className="form-group has-feedback">
+                <label htmlFor="signupInputPassword1" className="text-muted">
+                  Password
+                </label>
+                <input
+                  onChange={this.handlePassword}
+                  id="signupInputPassword1"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="off"
+                  required="required"
+                  className="form-control"
                 />
-              </a>
-            </div>
-            <div className="panel-body">
-              <p className="text-center pv">SIGNUP TO GET INSTANT ACCESS</p>
-              <form
-                id="register-form"
-                role="form"
-                data-parsley-validate=""
-                noValidate
-                onSubmit={this.handleSubmit}
-                className="mb-lg"
-              >
-                <div className="form-group has-feedback">
-                  <label htmlFor="signupInputEmail1" className="text-muted">
-                    Email address
+                <span className="fa fa-lock form-control-feedback text-muted" />
+              </div>
+              <div className="form-group has-feedback">
+                <label htmlFor="signupInputRePassword1" className="text-muted">
+                  Retype Password
+                </label>
+                <input
+                  id="signupInputRePassword1"
+                  type="password"
+                  placeholder="Retype Password"
+                  autoComplete="off"
+                  required="required"
+                  data-parsley-equalto="#signupInputPassword1"
+                  className="form-control"
+                />
+                <span className="fa fa-lock form-control-feedback text-muted" />
+              </div>
+              <div className="clearfix">
+                <div className="checkbox c-checkbox pull-left mt0">
+                  <label>
+                    <input
+                      type="checkbox"
+                      value=""
+                      required="required"
+                      name="agreed"
+                    />
+                    <em className="fa fa-check" />
+                    I agree with the
+                    {" "}
+                    <a href="#">terms</a>
                   </label>
-                  <input
-                    onChange={this.handleEmail}
-                    id="signupInputEmail1"
-                    type="email"
-                    placeholder="Enter email"
-                    autoComplete="off"
-                    required="required"
-                    className="form-control"
-                  />
-                  <span className="fa fa-envelope form-control-feedback text-muted" />
                 </div>
-                <div className="form-group has-feedback">
-                  <label htmlFor="signupInputPassword1" className="text-muted">
-                    Password
-                  </label>
-                  <input
-                    onChange={this.handlePassword}
-                    id="signupInputPassword1"
-                    type="password"
-                    placeholder="Password"
-                    autoComplete="off"
-                    required="required"
-                    className="form-control"
-                  />
-                  <span className="fa fa-lock form-control-feedback text-muted" />
-                </div>
-                <div className="form-group has-feedback">
-                  <label
-                    htmlFor="signupInputRePassword1"
-                    className="text-muted"
-                  >
-                    Retype Password
-                  </label>
-                  <input
-                    id="signupInputRePassword1"
-                    type="password"
-                    placeholder="Retype Password"
-                    autoComplete="off"
-                    required="required"
-                    data-parsley-equalto="#signupInputPassword1"
-                    className="form-control"
-                  />
-                  <span className="fa fa-lock form-control-feedback text-muted" />
-                </div>
-                <div className="clearfix">
-                  <div className="checkbox c-checkbox pull-left mt0">
-                    <label>
-                      <input
-                        type="checkbox"
-                        value=""
-                        required="required"
-                        name="agreed"
-                      />
-                      <em className="fa fa-check" />
-                      I agree with the
-                      {" "}
-                      <a href="#">terms</a>
-                    </label>
-                  </div>
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-block btn-primary mt-lg"
-                >
-                  Create account
-                </button>
-              </form>
-              <p className="pt-lg text-center">Have an account?</p>
-              <Link to="login" className="btn btn-block btn-default">
-                Signup
-              </Link>
-            </div>
+              </div>
+              <button type="submit" className="btn btn-block btn-primary mt-lg">
+                Create account
+              </button>
+            </form>
+            <p className="pt-lg text-center">Have an account?</p>
+            <Link to="login" className="btn btn-block btn-default">
+              Signup
+            </Link>
           </div>
-          {/* END panel */}
         </div>
-      );
-    }
+        {/* END panel */}
+      </div>
+    );
   }
 }
 
